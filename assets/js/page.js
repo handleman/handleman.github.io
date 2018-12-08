4ce9acb7a46eab111c6f9e4e99ce320789915f39
handleman <pavel.handleman@gmail.com>
2018-12-08 18:14:23 +0300

    move to static

--- assets/js/page.js
+++ assets/js/page.js
@@ -5,6 +5,10 @@
 			$(this).toggleClass('active');
 			$(this).next('.coll').toggleClass('active');
 		});
+		// $('.to-top-btn').click(function() {
+		// 	$('body').animate({scrollTop: 0},600);
+		// 	return false;
+		// });
 		$('.to-top-btn').click(function() {
 			$.scrollTo('.container', 800);
 			return false;
